f1b90ddc3e44489ceef56059a8d09a07b46d4a75
diego <openheimerdiego@gmail.com>
2017-06-06 15:46:50 -0300

    Teste no Hello world

--- Curso em video atividade-Java basico/HelloWorld/src/helloworld/HelloWorld.java
+++ Curso em video atividade-Java basico/HelloWorld/src/helloworld/HelloWorld.java
@@ -17,7 +17,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        JOptionPane.showMessageDialog(null, "Hello World!");
+        JOptionPane.showMessageDialog(null, "HELLO WORLD!");
         
     }
     
